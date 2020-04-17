0c50a6c39ab03876119efc2ca42b8959b343dfd9
j2000lbs <j2000lbs@gmail.com>
2020-04-17 16:55:22 -0500

    Initial Commit

---
+++ SwiftUI JETRoute 2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI JETRoute 2.0
+//
+//  Created by Joel Ton on 4/17/20.
+//  Copyright © 2020 RAMJETApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
